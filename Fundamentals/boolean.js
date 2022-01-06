5de7eb85a39501bd8073caf9c6aa20da624741bc
Christopher DeLeon <lionscantcode@gmail.com>
2022-01-05 21:51:00 -0600

    Created boolean.js & if-statement.js

---
+++ Fundamentals/boolean.js
@@ -0,0 +1,24 @@
+// === - equality operator 
+// !== - not equal 
+// < - less than operator 
+// <= - less than or equal to operator
+
+// Lesson:
+let temp = -30
+let isFreezing = temp <= 32
+console.log(isFreezing)
+
+
+
+// Challenge area
+
+// Create age variable set to your age
+let age = 6
+// calc is child? If they are 7 or under
+let isChild = age <= 7
+//calc is senior? If they are 65 or older
+let isSenior = age >= 65 
+//print is child value
+console.log("Is a child? " + isChild)
+//print is senior value
+console.log("Is a senior? " + isSenior)
