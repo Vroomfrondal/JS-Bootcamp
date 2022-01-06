b4a121ae06e6f213cb59599993c56b236624ba3d
Christopher DeLeon <lionscantcode@gmail.com>
2022-01-06 17:10:54 -0600

    Function work
    - finished exercise to convert F to C by calling a function with the argument, Fahrenheit
    
    - created undefined and null function example
    
    - created root folder for basics and functions

---
+++ Fundamentals/Functions/functions.js
@@ -0,0 +1,24 @@
+// Function - input (argument), logic code, output (return)
+function square(num) {
+    let result = num * num
+    return result
+}
+
+let value = square(2)
+let value1 = square(5)
+
+
+
+// challenge0 area
+// create function convert F to C
+// argument f and c
+// return c
+// print converted values
+
+function convertFahrenheitToCelsius(fahrenheit) {
+    let celsius =  5/9 * (fahrenheit - 32) 
+    
+    console.log(celsius = celsius.toFixed(2))
+}
+
+convertFahrenheitToCelsius(98)
---
+++ Fundamentals/Functions/undefined-null.js
@@ -0,0 +1,17 @@
+// Undefined keyword for variables,
+let name
+
+if (name === undefined) {
+    console.log("Please enter your name: ")
+} else {
+    console.log(name)
+}
+
+
+// Undefined for function arguments
+function square(num) {
+    console.log(num * num)
+} 
+
+
+
