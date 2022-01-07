643d9c90c30d44086a1b75f9cd40cc01907417ed
Christopher DeLeon <lionscantcode@gmail.com>
2022-01-07 15:05:29 -0600

    Update grade-calc.js
    - Created a grade calculator to (test exercise before moving on):
    
    1) calculate students test score.
    2) convert percentage test score to a letter grading system and
    3) display both the letter and score as a string

--- Fundamentals/Functions/grade-calc.js
+++ Fundamentals/Functions/grade-calc.js
@@ -1,44 +1,35 @@
 // Create a grade calculator function. arg1: students score. arg2: total possible score.
 // Ex: 15/20 -> You got a "C" (75%) (return a string)
 
+function main() {
+    calcGrade(17, 20) // Enter: student's score, total possible score on test  
+    determineGradeLetter() 
+}
+
+main()
+
 function calcGrade(studentScore, totalScorePossible) {
-    //calc score & convert to percentage
+    //calculate score & convert to percentage
     calculatedScore = (studentScore / totalScorePossible )
-    scoreConvertedToPercentage = (calculatedScore * 100)
+    scoreConvertedToPercentage = (calculatedScore * 100) 
 
     return scoreConvertedToPercentage
 }
 
 function determineGradeLetter() {
-    let A = {
-        from: 90,
-        to: 100,
-    }
-    let B = [89, 88, 87, 86, 85, 84, 83, 82, 81, 80]
-    let C = [79, 78, 77, 76, 75, 74, 73, 72, 71, 70]
-    let D = [69, 68, 67, 66, 65, 64, 63, 62, 61]
-    let F = { 
-        from: 0,
-        to: 59
-    }
-    
+    let percentage = scoreConvertedToPercentage
 
-    //if (score == A) {
-    //    console.log("You recieved an A! ")
-    //} else if(score == B) {
-    //    console.log("You recieved a B! ")
-    //}
-//
-    if (scoreConvertedToPercentage == A) {
-        let score = "A"
-        return score
+    // Determine what Letter corresponds with scoreConvertedToPercentage 
+    // Grading scale: 
+    if (percentage >= 90 && percentage <= 100) {
+        console.log("You made an A " + "with a " + percentage + "%" + "!")
+    } else if (percentage >= 80 && percentage <= 89) {
+        console.log("You made a B " + "with a " + percentage + "%" + "!")
+    } else if (percentage >= 70 && percentage <= 79) {
+        console.log("You made a C " + "with a " + percentage + "%" + "!")
+    } else if (percentage >= 60 && percentage <= 69) {
+        console.log("You made a D " + "with a " + percentage + "%" + "!")
+    } else if (percentage <= 60) {
+        console.log("You made a F " + "with a " + percentage + "%" + "!")
     }
 }
-
-function main() {
-    calcGrade(19, 20) // student's score, total possible score on test
-    determineGradeLetter()
-    console.log(score)
-}
-
-main()
