683590eb65cb489751a707e7f288b38d247d3dce
Christopher DeLeon <lionscantcode@gmail.com>
2022-01-10 21:17:46 -0600

    Create notes.js & todo.js
    Two projects

---
+++ Fundamentals/Arrays/notes.js
@@ -0,0 +1,15 @@
+const notes = ["Note 1", "Note 2", "Note 3"]
+
+//console.log(notes.pop())
+//notes.push("Note 4")
+//
+//console.log(notes.shift()) // removes first element from array
+//notes.unshift("My first note")
+
+//read documentation on splice()
+//notes.splice(1, 1) //start at index 1 and remove that item
+//notes.splice(1, 0, "This is the new second item") //adds item to the middle
+
+
+//console.log(notes.length)
+//console.log(notes)
---
+++ Fundamentals/Objects/constants.js
@@ -0,0 +1,6 @@
+const person = { 
+    age: 23
+}
+
+person.age += 1
+console.log(person)
