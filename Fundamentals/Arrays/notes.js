7e94862edebbf2a10f56bc2c4741ad6a65a9abda
Christopher DeLeon <lionscantcode@gmail.com>
2022-01-14 20:09:19 -0600

    Update notes.js and todo.js

--- Fundamentals/Arrays/notes.js
+++ Fundamentals/Arrays/notes.js
@@ -10,34 +10,34 @@ const notes = [
   },
   {
     title: "Intern Checklist",
-    body: "Daily tasks that need done at the internship.",
+    body: "Clock in, Get Parking Token, Daily Logs",
   },
 ]
 
-//console.log(notes.pop())
-//notes.push("Note 4")
+function findNote(notes, noteTitle) {
+  return notes.find(function(note, index) {
+    return note.title.toLowerCase() === noteTitle.toLowerCase()
+  })
+}
 
-//console.log(notes.shift()) // removes first element from array
-//notes.unshift("My first note")
+// function findNote(notes, noteTitle) {
+//   const index = notes.findIndex(function(note, index) {
+//     return note.title.toLowerCase() === noteTitle.toLowerCase()
+//   })
+//   return notes[index]
+// }
 
-//read documentation on splice()
-//notes.splice(1, 1) //start at index 1 and remove that item
-//notes.splice(1, 0, "This is the new second item") //adds item to the middle
+const note = findNote(notes, "Intern Checklist")
+console.log(note)
 
-notes[2] = "this is my new note 3"
 
-// function that determines what to do with each item in the array (callback function)
-notes.forEach(function (item, index) {
-  console.log(index)
-  console.log(item)
-})
 
-console.log(notes.length)
-console.log(notes)
-console.log("----------------------------------")
+// console.log(notes.length)
+// console.log(notes)
+// console.log("----------------------------------")
 
-const index = notes.findIndex(function (note, index) {
-  console.log(note)
-  return note.title === "Intern Checklist"
-})
-console.log(index)
+// const index = notes.findIndex(function (note, index) {
+//   console.log(note)
+//   return note.title === "Intern Checklist"
+// })
+// console.log(index)
--- Fundamentals/Arrays/todo.js
+++ Fundamentals/Arrays/todo.js
@@ -1,28 +1,25 @@
 const todo = ["Order food", "Groceries", "Workout", "Internship", "School"]
 
-// delete third item
-// add new item to end
-// remove the first item from the list
-todo.splice(2, 1)
-todo.push("New item")
-todo.shift()
-console.log(`You have ${todo.length} todo's!`)
+const todo = [{
+    text: "Order Food",
+    completed: false,
+}, {
+    text: "Groceries",
+    completed: true,
+}, {
+    text: "Workout",
+    completed: true,
+}, {
+    text: "Internship",
+    completed: true,
+}, {
+    text: "School",
+    completed: false,
+}]
 
+// 1. Convert array of strings to objects --> Properties: text, completed?
+// 2. Create function that allows you to remove todo by text value (case sensitive)
+    // steps: find index of item, if found, delete the item.
 
-// forEach() Method
-// print 1) The first item
-// " 2) the second item
-// " 3) the "new item"
-todo.forEach(function(item, index) {
-    console.log(` ${index + 1}. ${item}`)
-    
-})
-
-
-console.log(`-----------`)
-
-// for Loop method
-for (items = 0; items < todo.length; items++) {
-    const indexTracker = items + 1
-    console.log(`${indexTracker}. ${todo[items]}`)
-}
+deleteTodos(todos, "remove this todo")
+console.log(todos)
