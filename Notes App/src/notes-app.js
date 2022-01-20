d65fd2e121c7da535d71a0dfc1c738e2341c6481
Christopher DeLeon <lionscantcode@gmail.com>
2022-01-19 22:29:42 -0600

    Update notes.js
    added button and event listener for adding new note

--- Notes App/src/notes-app.js
+++ Notes App/src/notes-app.js
@@ -13,24 +13,6 @@ const notes = [
     },
 ]
 
-// Query & Remove single paragraph tag
-//const paragraph = document.querySelector("p")
-//paragraph.remove()
-
-// Query & Remove multiple tags
-const multiParagraphs = document.querySelectorAll("p")
-
-multiParagraphs.forEach(function (paragraph) {
-    //rename text in paragraph tags
-    paragraph.textContent = "*********"
-    //console.log(paragraph.textContent)
-    //paragraph.remove()
+document.getElementById("create-note-button").addEventListener("click", function (e) {
+    e.target.textContent = "The button was clicked."
 })
-
-// Add a new element:
-// 1) select an area
-const newParagraph = document.createElement("p")
-// 2) give it content
-newParagraph.textContent = "Newest element from JS"
-// 3)select body and append new text, newParagraph (step 2)
-document.querySelector("body").appendChild(newParagraph)
