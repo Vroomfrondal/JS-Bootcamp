2632b8f4166f2fb47275965260ddb3b90a60f68b
Christopher DeLeon <lionscantcode@gmail.com>
2022-02-03 21:03:30 -0600

    Notes.app Refactor
    Refactored multiple functions within notes-main.js and relocated to notes-functions.js where all functions are stored

--- Notes App/src/notes-functions.js
+++ Notes App/src/notes-functions.js
@@ -1,1 +1,41 @@
-console.log(filters)
+// Read Existing notes from local storage
+function getSavedNotes() {
+    const notesJSON = localStorage.getItem("notes")
+
+    // read & parse data when app starts up
+    if (notesJSON !== null) {
+        console.log("Notes Retrieved from Local Storage.")
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
+
+// generate DOM structure for a new note
+function generateNoteDOM(note) {
+    const noteElement = document.createElement("p")
+
+    if (note.title.length > 0) {
+        noteElement.textContent = note.title
+    } else {
+        noteElement.textContent = "New Note"
+    }
+    return noteElement
+}
+
+// render application notes
+// return a new array with notes that are included in our filter object, searchText: ""
+function renderNotes(notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    // clear notes div before adding new filteredNotes to list
+    document.querySelector("#filtered-notes-by-searchText").innerHTML = ""
+
+    //itterate over filtered notes array and create new element for each note title
+    filteredNotes.forEach(function (note) {
+        const noteElement = generateNoteDOM(note)
+        document.querySelector("#filtered-notes-by-searchText").appendChild(noteElement)
+    })
+}
--- Notes App/src/notes-main.js
+++ Notes App/src/notes-main.js
@@ -1,42 +1,7 @@
-let notes = []
+let notes = getSavedNotes() //notes-function.js
 
 const filters = {
     searchText: "",
-}
-
-// Check for existing saved data
-const notesJSON = localStorage.getItem("notes")
-
-// read & parse data when app starts up
-if (notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-
-const userJSON = localStorage.getItem("user")
-const user = JSON.parse(userJSON)
-console.log(`${userJSON.name} is ${userJSON.age}`)
-
-// return a new array with notes that are included in our filter object, searchText: ""
-function renderNotes(notes, filters) {
-    const filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    // clear notes div before adding new filteredNotes to list
-    document.querySelector("#filtered-notes-by-searchText").innerHTML = ""
-
-    //itterate over filtered notes array and create new element for each note title
-    filteredNotes.forEach(function (note) {
-        const noteElement = document.createElement("p")
-
-        if (note.title.length > 0) {
-            noteElement.textContent = note.title
-        } else {
-            noteElement.textContent = "New Note"
-        }
-
-        document.querySelector("#filtered-notes-by-searchText").appendChild(noteElement)
-    })
 }
 
 renderNotes(notes, filters)
@@ -49,7 +14,6 @@ document.querySelector("#create-note-button").addEventListener("click", function
     })
     localStorage.setItem("notes", JSON.stringify(notes))
     renderNotes(notes, filters)
-    document.querySelector("#new-todo")
 })
 
 // event listener to update filteredNotes and re Render function
