584e533fae39d3278da4538dfd98d3aa561c4e8c
Christopher DeLeon <lionscantcode@gmail.com>
2022-02-08 21:27:26 -0600

    moment.js Date fun
    learning dates

--- Notes App/src/notes-main.js
+++ Notes App/src/notes-main.js
@@ -36,3 +36,16 @@ window.addEventListener("storage", function (e) {
     renderNotes(notes, filters)
     console.log("Data Updated.")
 })
+
+// const now = moment()
+
+// now.add("1", "year").subtract(20, "days")
+// console.log(now.format("MMMM Do, YYYY"))
+// console.log(now.fromNow())
+
+// const nowTimeStamp = now.valueOf()
+// console.log(moment(nowTimeStamp).toString())
+
+const birthday = moment()
+birthday.set("year", 1998).set("month", 5).set("date", 29)
+console.log(birthday.format("MMM D, YYYY"))
