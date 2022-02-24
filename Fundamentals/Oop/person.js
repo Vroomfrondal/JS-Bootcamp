f413a0aa024aefb034b1e8b4148d46db8a1e5bdc
Christopher DeLeon <lionscantcode@gmail.com>
2022-02-23 22:16:11 -0600

    'Hangman' class creation
    - created class Hangman instead of individual prototyped methods on the constructor function, "Hangman"

--- Fundamentals/Oop/person.js
+++ Fundamentals/Oop/person.js
@@ -1,32 +1,32 @@
 //Prototypal Inheritance
 
-// constructor function
-function Person(firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
+class Person {
+    //constructor function
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-
-    return bio
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(" ")
-    this.firstName = names[0]
-    this.lastName = names[1]
+        return bio
+    }
+    setName(fullName) {
+        const names = fullName.split(" ")
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
 //instance 1
-const me = new Person("Chris", "Deleon", 23, ["Lifting", "Coding"])
-me.setName("Tuff Hale")
+const me = new Person("Chris", "Deleon", 23, ["power lifting", "coding"])
+//me.setName("Tuff Hale")
 console.log(me.getBio())
 
 //instance 2
--- Hangman App/src/hangman.js
+++ Hangman App/src/hangman.js
@@ -1,57 +1,53 @@
-function Hangman(word, remainingGuesses) {
-    //setup instance properties - first as array of lowercase letters so we can loop through
-    this.word = word.toLowerCase().split("")
-    this.remainingGuesses = remainingGuesses
-    this.guessedLetters = []
-    this.status = "Playing"
-}
-
-Hangman.prototype.getPuzzle = function () {
-    let puzzle = ""
-
-    // loop through game-word and sensor each letter that's not already been guessed
-    this.word.forEach((letter) => {
-        // check if letters in game-word have been guessed already and sensor those that haven't
-        if (this.guessedLetters.includes(letter)) {
-            puzzle += letter
-        } else {
-            puzzle += "*"
-        }
-    })
-
-    return puzzle
-}
-
-Hangman.prototype.makeGuess = function (guess) {
-    guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const badGuess = !this.word.includes(guess)
-
-    // Make sure guess is unique.
-    if (isUnique) {
-        //console.log("This is a unique guess.")
-        this.guessedLetters.push(guess)
-    }
-
-    if (isUnique && badGuess) {
-        //console.log("But an incompatible guess")
-        this.remainingGuesses--
-    }
-
-    //this.getStatus()
-}
-
-// Status-property of the Hangman game Logic
-Hangman.prototype.getStatus = function () {
-    const solved = !this.getPuzzle().includes("*")
-
-    if (this.remainingGuesses <= 0) {
-        this.status = "Game Over :["
-    } else if (solved) {
-        this.status = "You Won!"
-    } else {
+class Hangman {
+    //constructor function
+    constructor(word, remainingGuesses) {
+        //setup instance properties - first as array of lowercase letters so we can loop through
+        this.word = word.toLowerCase().split("")
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = []
         this.status = "Playing"
     }
+    getPuzzle() {
+        let puzzle = ""
 
-    return this.status
+        // loop through game-word and sensor each letter that's not already been guessed
+        this.word.forEach((letter) => {
+            // check if letters in game-word have been guessed already and sensor those that haven't
+            if (this.guessedLetters.includes(letter)) {
+                puzzle += letter
+            } else {
+                puzzle += "*"
+            }
+        })
+        return puzzle
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const badGuess = !this.word.includes(guess)
+
+        // Make sure guess is unique.
+        if (isUnique) {
+            //console.log("This is a unique guess.")
+            this.guessedLetters.push(guess)
+        }
+
+        if (isUnique && badGuess) {
+            //console.log("But an incompatible guess")
+            this.remainingGuesses--
+        }
+    }
+    getStatus() {
+        const solved = !this.getPuzzle().includes("*")
+
+        if (this.remainingGuesses <= 0) {
+            this.status = `Game Over :[ Correct Word: "${this.word.join("")}"`
+        } else if (solved) {
+            this.status = "You Won!"
+        } else {
+            this.status = "Playing"
+        }
+
+        return this.status
+    }
 }
