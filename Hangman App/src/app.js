4f9cbf9e0be880e477f5e73769fbe65593334a03
Christopher DeLeon <lionscantcode@gmail.com>
2022-02-27 20:48:31 -0600

    HTTP Requests for random puzzle/word
    - [CSS] changed background color
    
    - [JS] created XMLHttpRequest() to request random word for hangman game
    
    - [JS] added error-control when request is bad
    
    - [JS] retrieved country code

--- Hangman App/src/app.js
+++ Hangman App/src/app.js
@@ -21,3 +21,38 @@ window.addEventListener("keypress", (e) => {
     remainingGuessesEl.textContent = game1.remainingGuesses
     statusEl.textContent = game1.statusMessage
 })
+
+// Making HTTP Request
+// const fetchData = new XMLHttpRequest()
+
+// fetchData.addEventListener("readystatechange", (e) => {
+//     // ensure JSON is in "ready state value 4" and request is status "ok"
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         console.log(data)
+//     } else if (e.target.readyState === 4) {
+//         console.log("Request Sprung Error")
+//     }
+// })
+
+// fetchData.open("GET", "https://puzzle.mead.io/puzzle?wordCount=2")
+// fetchData.send()
+
+// Challenge
+
+const countryCode = "US"
+const fetchData = new XMLHttpRequest()
+
+fetchData.addEventListener("readystatechange", (e) => {
+    // look through array for countryCode and return that object
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.cca2 === countryCode)
+        console.log(country.name.official)
+    } else if (e.target.readyState === 4) {
+        console.log("Not a valid Country Code")
+    }
+})
+
+fetchData.open("GET", "https://restcountries.com/v3.1/all")
+fetchData.send()
